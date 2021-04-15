14456ec58f9181e11afdc23cf4d94c633eaaf539
Linux <1130018485@qq.com>
2021-04-15 20:50:51 +0800

    signal

---
+++ sign/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+
+int main()
+{
+    printf("process start\n");
+    raise(2);
+    printf("hahahahh\n");
+    kill(getpid(), 2);
+    while(1)
+    {
+        sleep(1);
+    }
+    return 0;
+}
